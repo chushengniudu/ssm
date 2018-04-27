77061a8de606d6e775e73b957568172fd3f0f224
Administrator <Administrator@SKY-LZ>
2018-04-27 17:24:57 +0800

    修正resources中的xml

--- src/main/java/cn/temptation/dao/UserDao.java
+++ src/main/java/cn/temptation/dao/UserDao.java
@@ -3,5 +3,5 @@ package cn.temptation.dao;
 import cn.temptation.entity.User;
 
 public interface UserDao {
-	public abstract User findByUsername(String username);
+	User findByUsername(String username);
 }
